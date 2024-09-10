0826882e8db87fe227353d6ff13d987648d1c7ad
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2024-09-10 13:14:29 -0300

    update image

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ let personagensHouseOfTheDragon = [
         descricao: "O irmão mais novo de Viserys I Targaryen e um guerreiro habilidoso. Carismático e impulsivo, ele é um dos personagens mais complexos de House of the Dragon, capaz tanto de grande heroísmo quanto de atos cruéis.",
         casa: "Targaryen",
         link: "https://wiki.geloefogo.com/index.php/Aemond_Targaryen",
-        image: "./assets/Daemon_profile.jpg"
+        image: "./assets/daemon_targaryen.jpg"
     },
     {
         nome: "Otto Hightower",
@@ -13,7 +13,7 @@ let personagensHouseOfTheDragon = [
         descricao: "O pai de Alicent Hightower e um manipulador astuto. Ele busca proteger os interesses de sua casa e de sua família, muitas vezes entrando em conflito com Rhaenyra Targaryen.",
         casa: "Hightower",
         link: "https://wiki.geloefogo.com/index.php/Otto_Hightower",
-        image: "https://wiki.geloefogo.com/images/0/0b/Otto_Hightower_HOTD.jpg"
+        image: "./assets/otto_hightower.jpg"
     },
     {
         nome: "Corlys Velaryon",
@@ -21,7 +21,7 @@ let personagensHouseOfTheDragon = [
         descricao: "Lorde Corlys Velaryon, conhecido como Serpente do Mar, foi um lendário Senhor das Marés, Mestre de Derivamarca e chefe da Casa Velaryon. Ele era o marido da princesa Rhaenys Targaryen. Durante a Dança dos Dragões, ele se tornou Mão da Rainha Rhaenyra Targaryen.",
         casa: "Velaryon",
         link: "https://wiki.geloefogo.com/index.php/Corlys_Velaryon",
-        image: "https://wiki.geloefogo.com/images/c/c4/Corlys_Velaryon_HOTD.png"
+        image: "./assets/corlys_velaryon.png"
     },
     {
         nome: "Aemond Targaryen",
@@ -29,7 +29,7 @@ let personagensHouseOfTheDragon = [
         descricao: "O filho mais novo de Alicent Hightower e Viserys I Targaryen. Ambicioso e cruel, ele se torna um dos principais antagonistas de Rhaenyra Targaryen.",
         casa: "Targaryen",
         link: "https://wiki.geloefogo.com/index.php/Aemond_Targaryen",
-        image: "https://wiki.geloefogo.com/images/a/ad/Aemond_Targaryen_HotD.jpg"
+        image: "./assets/aemond_targaryen.jpg"
     },
     {
         nome: "Rhaenyra Targaryen",
@@ -37,7 +37,7 @@ let personagensHouseOfTheDragon = [
         descricao: "Filha primogênita de Viserys I Targaryen, nomeada herdeira do trono. Sua reivindicação ao Trono de Ferro é o ponto central da Dança dos Dragões.",
         casa: "Targaryen",
         link: "https://wiki.geloefogo.com/index.php/Rhaenyra_Targaryen",
-        image: "https://wiki.geloefogo.com/images/7/7a/EMMA_DARCY_RHAENYRA.jpg"
+        image: "./assets/rhaenyra_targaryen.jpg"
     },
     {
         nome: "Alicent Hightower",
@@ -45,7 +45,7 @@ let personagensHouseOfTheDragon = [
         descricao: "Filha de Otto Hightower, casada com Viserys I Targaryen. Alicent é uma figura central nas intrigas políticas, buscando garantir o trono para seus filhos.",
         casa: "Hightower",
         link: "https://wiki.geloefogo.com/index.php/Alicent_Hightower",
-        image: "https://wiki.geloefogo.com/images/1/16/OLIVIA_COOKE_ALICENT.png"
+        image: "./assets/alicent_hightower.png"
     },
     {
         nome: "Viserys I Targaryen",
@@ -53,7 +53,7 @@ let personagensHouseOfTheDragon = [
         descricao: "Rei de Westeros durante o período que antecede a Dança dos Dragões. Seu reinado é marcado pela tensão sobre sua sucessão.",
         casa: "Targaryen",
         link: "https://wiki.geloefogo.com/index.php/Alicent_Hightower",
-        image: "https://wiki.geloefogo.com/images/8/8b/Viserys_I_House_Dragon.png"
+        image: "./assets/viserys_1_targaryen.png"
     },
     {
         nome: "Criston Cole",
@@ -61,6 +61,6 @@ let personagensHouseOfTheDragon = [
         descricao: "Um cavaleiro de origem comum que se torna uma figura central na guerra civil. Inicialmente leal a Rhaenyra, ele eventualmente se alinha com Alicent e seus filhos.",
         casa: "Cole",
         link: "https://wiki.geloefogo.com/index.php/Criston_Cole",
-        image: "https://wiki.geloefogo.com/images/a/a9/Criston_Cole_HoTD.jpg"
+        image: "./assets/criston_cole.jpg"
     }
 ];
