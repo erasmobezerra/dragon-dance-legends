4351f6bf9ecf7bf92db79cc6d7513726d35417ce
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2024-09-09 22:00:14 -0300

    add project

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>É obrigatório informar o nome ou título de um personagem da serie.</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = ""; 
+    let nome = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let personagem of personagensHouseOfTheDragon) {        
+        nome = personagem.nome.toLowerCase()
+        titulo = personagem.titulo.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (nome.includes(campoPesquisa) || titulo.includes(campoPesquisa)){
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <img src=${personagem.image} alt="image-daemon">
+                <h2>
+                    <a href="#" target="_blank">${personagem.nome}</a>
+                </h2>
+                <p class="descricao-meta">Título: ${personagem.titulo}</p>                
+                <p class="descricao-meta">Casa: ${personagem.casa}</p>    
+                <p class="descricao-meta">Descrição: ${personagem.descricao}</p>            
+                <p class="descricao-meta">Mais informações,<a href=${personagem.link} target="_blank"> cliquei aqui!</a></p>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Opsss! Você não digitou um nome ou título de um personagem da série. Favor tente novamente!</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ data.js
@@ -0,0 +1,66 @@
+let personagensHouseOfTheDragon = [
+    {
+        nome: "Daemon Targaryen",
+        titulo: "Príncipe de Pedra do Dragão",
+        descricao: "O irmão mais novo de Viserys I Targaryen e um guerreiro habilidoso. Carismático e impulsivo, ele é um dos personagens mais complexos de House of the Dragon, capaz tanto de grande heroísmo quanto de atos cruéis.",
+        casa: "Targaryen",
+        link: "https://wiki.geloefogo.com/index.php/Aemond_Targaryen",
+        image: "https://wiki.geloefogo.com/images/c/c0/Daemon_profile.jpg"
+    },
+    {
+        nome: "Otto Hightower",
+        titulo: "Mão do Rei",
+        descricao: "O pai de Alicent Hightower e um manipulador astuto. Ele busca proteger os interesses de sua casa e de sua família, muitas vezes entrando em conflito com Rhaenyra Targaryen.",
+        casa: "Hightower",
+        link: "https://wiki.geloefogo.com/index.php/Otto_Hightower",
+        image: "https://wiki.geloefogo.com/images/0/0b/Otto_Hightower_HOTD.jpg"
+    },
+    {
+        nome: "Corlys Velaryon",
+        titulo: "Serpente do Mar",
+        descricao: "Lorde Corlys Velaryon, conhecido como Serpente do Mar, foi um lendário Senhor das Marés, Mestre de Derivamarca e chefe da Casa Velaryon. Ele era o marido da princesa Rhaenys Targaryen. Durante a Dança dos Dragões, ele se tornou Mão da Rainha Rhaenyra Targaryen.",
+        casa: "Velaryon",
+        link: "https://wiki.geloefogo.com/index.php/Corlys_Velaryon",
+        image: "https://wiki.geloefogo.com/images/c/c4/Corlys_Velaryon_HOTD.png"
+    },
+    {
+        nome: "Aemond Targaryen",
+        titulo: "Príncipe",
+        descricao: "O filho mais novo de Alicent Hightower e Viserys I Targaryen. Ambicioso e cruel, ele se torna um dos principais antagonistas de Rhaenyra Targaryen.",
+        casa: "Targaryen",
+        link: "https://wiki.geloefogo.com/index.php/Aemond_Targaryen",
+        image: "https://wiki.geloefogo.com/images/a/ad/Aemond_Targaryen_HotD.jpg"
+    },
+    {
+        nome: "Rhaenyra Targaryen",
+        titulo: "Rainha dos Sete Reinos",
+        descricao: "Filha primogênita de Viserys I Targaryen, nomeada herdeira do trono. Sua reivindicação ao Trono de Ferro é o ponto central da Dança dos Dragões.",
+        casa: "Targaryen",
+        link: "https://wiki.geloefogo.com/index.php/Rhaenyra_Targaryen",
+        image: "https://wiki.geloefogo.com/images/7/7a/EMMA_DARCY_RHAENYRA.jpg"
+    },
+    {
+        nome: "Alicent Hightower",
+        titulo: "Rainha Consorte",
+        descricao: "Filha de Otto Hightower, casada com Viserys I Targaryen. Alicent é uma figura central nas intrigas políticas, buscando garantir o trono para seus filhos.",
+        casa: "Hightower",
+        link: "https://wiki.geloefogo.com/index.php/Alicent_Hightower",
+        image: "https://wiki.geloefogo.com/images/1/16/OLIVIA_COOKE_ALICENT.png"
+    },
+    {
+        nome: "Viserys I Targaryen",
+        titulo: "Rei dos Sete Reinos",
+        descricao: "Rei de Westeros durante o período que antecede a Dança dos Dragões. Seu reinado é marcado pela tensão sobre sua sucessão.",
+        casa: "Targaryen",
+        link: "https://wiki.geloefogo.com/index.php/Alicent_Hightower",
+        image: "https://wiki.geloefogo.com/images/8/8b/Viserys_I_House_Dragon.png"
+    },
+    {
+        nome: "Criston Cole",
+        titulo: "Lord Comandante da Guarda Real",
+        descricao: "Um cavaleiro de origem comum que se torna uma figura central na guerra civil. Inicialmente leal a Rhaenyra, ele eventualmente se alinha com Alicent e seus filhos.",
+        casa: "Cole",
+        link: "https://wiki.geloefogo.com/index.php/Criston_Cole",
+        image: "https://wiki.geloefogo.com/index.php/Arquivo:Criston_Cole_HoTD.jpg"
+    }
+];
